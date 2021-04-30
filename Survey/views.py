17074f814859465c072312ae38d193b63d1cb8a4
GhostFantik <ghostfantik@gmail.com>
2021-04-30 13:30:58 +0300

    small fix survey's ordering

--- Survey/views.py
+++ Survey/views.py
@@ -7,7 +7,7 @@ from Auth.permissions import (IsCarPermission, IsAdminPermission, IsDriverPermis
 
 # GET - ADMIN, POST - Car, Driver, Admin. Remove PATCH, DELETE query
 class SurveyViewSet(ModelViewSet):
-    queryset = Survey.objects.order_by('date', '-end_time')
+    queryset = Survey.objects.order_by('-date', '-end_time')
     serializer_class = SurveySerializer
     permission_classes = [IsCarPermission|IsDriverPermission|IsAdminPermission|IsAdminUser]
     http_method_names = ['get', 'post', 'head', 'options']
