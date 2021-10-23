155b9825eb472b608577823ae081706395383d49
GhostFantik <ghostfantik@gmail.com>
2021-10-23 15:13:32 +0500

    password hash #1

--- Auth/admin.py
+++ Auth/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import *
 
-admin.site.register(User)
+admin.site.register(User, UserAdmin)
 admin.site.register(Driver)
 admin.site.register(Mechanic)
 admin.site.register(Admin)
