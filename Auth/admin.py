b9fff177d603ea7efcae0655dfce5aceee9fc45e
GhostFantik <ghostfantik@gmail.com>
2021-10-23 15:40:56 +0500

    password hash #2

--- Auth/admin.py
+++ Auth/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import *
 
+class CustomUserAdmin(UserAdmin):
+    fields = ('is_superuser', 'first_name', 'last_name', 'email', 'is_staff', 'is_active')
+
 admin.site.register(User, UserAdmin)
 admin.site.register(Driver)
 admin.site.register(Mechanic)
