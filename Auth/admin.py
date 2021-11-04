50ffddc4b5042c84baf28b223ce8d90e942d1dec
GhostFantik <ghostfantik@gmail.com>
2021-11-04 14:47:53 +0500

    smth

--- Auth/admin.py
+++ Auth/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import *
 
+
 admin.site.register(User)
 admin.site.register(Driver)
 admin.site.register(Mechanic)
